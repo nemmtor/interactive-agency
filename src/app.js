8e232a60827a3f3e42c2534036e59d49d1c368b1
nemmtor <kacper0witas@gmail.com>
2020-10-12 23:45:48 +0200

    Adds more styles and animations

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
 import './utils/loadingscreen';
 import './utils/scrolling';
+
+AOS.init({ once: true, duration: 800 });
