1108aa23b553c0f55cf6cd4dd23f6935f4d088aa
nemmtor <kacper0witas@gmail.com>
2020-10-13 14:22:56 +0200

    whatwedo and best sections finished

--- src/utils/scrolling.js
+++ src/utils/scrolling.js
@@ -1,15 +1,15 @@
-const burger = document.querySelector('.nav__burger');
-const nav = document.querySelector('.nav');
-const scrollTriggers = document.querySelectorAll('[data-scroll]');
+const burger = document.querySelector(".nav__burger");
+const nav = document.querySelector(".nav");
+const scrollTriggers = document.querySelectorAll("[data-scroll]");
 
-burger.addEventListener('click', () => {
-  nav.classList.toggle('nav--open');
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav--open");
 });
 
 scrollTriggers.forEach((trigger) => {
-  trigger.addEventListener('click', () => {
-    nav.classList.toggle('nav--open');
-    const href = trigger.getAttribute('href');
+  trigger.addEventListener("click", () => {
+    nav.classList.remove("nav--open");
+    const href = trigger.getAttribute("href");
     window.location = href;
   });
 });
