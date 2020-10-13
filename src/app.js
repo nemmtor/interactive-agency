3bf8532dd54f9d9a2bb5f8e2eef79fb180cd8165
nemmtor <kacper0witas@gmail.com>
2020-10-13 14:44:11 +0200

    more styles

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
-import './utils/loadingscreen';
-import './utils/scrolling';
+import "./utils/loadingscreen";
+import "./utils/scrolling";
+import "./utils/slider";
 
 AOS.init({ once: true, duration: 800 });
