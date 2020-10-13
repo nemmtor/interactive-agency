3bfa29c579efcca5d585128282882eef47cbceeb
nemmtor <kacper0witas@gmail.com>
2020-10-13 20:02:37 +0200

    compress images

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 import 'normalize.css';
 import './styles.css';
 
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-
 import './utils/loadingscreen';
 import './utils/slider';
 import './utils/scrolling';
-
-AOS.init({ once: true, duration: 800 });
--- src/utils/scrolling.js
+++ src/utils/scrolling.js
@@ -1,16 +1,21 @@
-const burger = document.querySelector(".nav__burger");
-const nav = document.querySelector(".nav");
-const scrollTriggers = document.querySelectorAll("[data-scroll]");
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("nav--open");
+AOS.init({ once: true, duration: 800 });
+
+const burger = document.querySelector('.nav__burger');
+const nav = document.querySelector('.nav');
+const scrollTriggers = document.querySelectorAll('[data-scroll]');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav--open');
 });
 
 scrollTriggers.forEach((trigger) => {
-  trigger.addEventListener("click", (e) => {
+  trigger.addEventListener('click', (e) => {
     e.preventDefault();
-    nav.classList.remove("nav--open");
-    const href = trigger.getAttribute("href");
+    nav.classList.remove('nav--open');
+    const href = trigger.getAttribute('href');
     window.location = href;
   });
 });
